b9fabba10a7c2ce02dc6e9f2da95c81a5f6fc2ad
Gustav <meckan@hotmail.se>
2022-02-21 13:59:03 +0100

    initial commit: basic setup from spring initializer

---
+++ src/main/java/com/example/thymeleafsong/ThymeleafSongApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafsong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafSongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thymeleafsong/ThymeleafSongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafsong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
